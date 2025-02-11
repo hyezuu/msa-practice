5a4aa499a8efcf9464631609661bf2fc4cfc0763
hyezuu <duudu0704@gmail.com>
2025-02-11 12:14:33 +0900

    Spring-Cloud-Config 설정 추가

--- com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -2,16 +2,22 @@ package com.spring_cloud.eureka.client.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+//IOC 컨테이너가 빈 객체 생성시에, @Value 값들을 주입하는데 -> 변경 사항이 생기면 빈을 재생성해야함.
+@RefreshScope
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
 
 	@Value("${server.port}")
 	private String serverPort;
+
+	@Value("${message}")
+	private String message;
 
 	private final ProductService productService;
 
@@ -23,6 +29,6 @@ public class ProductController {
 
 	@GetMapping("/product")
 	public String getProduct() {
-		return "ProductInfo ! \n From port : " + serverPort;
+		return "ProductInfo ! \n From port : " + serverPort + "and message : " + message;
 	}
 }
